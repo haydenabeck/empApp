1ea90b91b0b1e2e5b05221e934326dba885a11b7
poornimasewak <poornima.mathur.20@gmail.com>
2021-06-03 12:46:32 -0500

    added client1

---
+++ client1/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import EmpApp from './components/EmpApp';
+import EmpList from './pages/EmpList';
+
+function App() {
+    return (
+        <Router>
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        nav test again
+                    </div>
+                    <div className="col-9">
+
+
+                        <Switch>
+                            <Route exact path={"/"}>
+                                <EmpApp />
+                            </Route>
+                            <Route exact path={"/emplist"}>
+                                <EmpList />
+                            </Route>
+
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
